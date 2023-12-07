46458b2258a91c843b3de8c4a2a76c29f774956b
zhyun-pivotal <52663478+zhyun-pivotal@users.noreply.github.com>
2023-12-07 18:45:26 +0900

    Update diag_gpupgrade.sh

--- diag_gpupgrade.sh
+++ diag_gpupgrade.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #mkdir -p /home/gpadmin/diag/diaglog
+source /usr/local/greenplum-db/greenplum_path.sh
 export LOGFILE=/home/gpadmin/diag/diaglog/diag_gpupgrade.$(date '+%Y%m%d_%H%M')
 export HOSTFILE=/home/gpadmin/diag/hostfile_all
 
