7c47254072c83f12be695afbef7c2a33c8999731
zhyun-pivotal <52663478+zhyun-pivotal@users.noreply.github.com>
2023-12-07 18:45:38 +0900

    Update diag_perf_dbstatus.sh

--- diag_perf_dbstatus.sh
+++ diag_perf_dbstatus.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #mkdir -p /home/gpadmin/diag/diaglog
+source /usr/local/greenplum-db/greenplum_path.sh
 export LOGFILE=/home/gpadmin/diag/diaglog/diag_perf_dbstatus.$(date '+%Y%m%d_%H%M')
 
 echo "" > ${LOGFILE}
