f68276570400790e510d300f374c1942457e2756
zhyun-pivotal <52663478+zhyun-pivotal@users.noreply.github.com>
2023-12-07 13:57:21 +0900

    Create chk_bloat_tables.sh

---
+++ chk_bloat_tables.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+mkdir -p /home/gpadmin/dba/diaglog
+export LOGFILE=/home/gpadmin/dba/diaglog/chk_bloat_tables.$(date '+%Y%m%d_%H%M')
+
+psql -c "
+SELECT a.*
+FROM (
+    SELECT n.nspname as schemaname,
+        c.relname as tablename,
+        b.btdrelpages as actual_pages,
+        b.btdexppages as expected_pages,
+        round(pg_relation_size(n.nspname || ‘.’ || c.relname)/1024/1024) actual_size_mb,
+        round(pg_relation_size(n.nspname || ‘.’ || c.relname)/1024/1024 * b.btdexppages/b.btdrelpages) expected_size_mb
+    FROM gp_toolkit.gp_bloat_expected_pages b
+    JOIN pg_class c ON c.relnamespace = n.oid
+    WHERE 1=1
+    -- AND b.btdrelpages > 1
+    AND n.nspname not in (‘pg_catalog’, 'gp_toolkit', 'information_schema') ) a
+WHERE 1=1
+-- AND actual_size_mb > 1
+-- AND actual_size_mb / expected_size_mb > 1
+ORDER BY 1,2 desc;"
