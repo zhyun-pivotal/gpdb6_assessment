ee5c7d1e46b6eb4f8b35b21a51c70ee68e81224c
zhyun-pivotal <52663478+zhyun-pivotal@users.noreply.github.com>
2023-12-07 16:20:12 +0900

    Update chk_bloat_tables.sh

--- chk_bloat_tables.sh
+++ chk_bloat_tables.sh
@@ -3,20 +3,21 @@
 export LOGFILE=/home/gpadmin/diag/diaglog/chk_bloat_tables.$(date '+%Y%m%d_%H%M')
 
 psql -c "
-SELECT a.*
-FROM (
-    SELECT n.nspname as schemaname,
-        c.relname as tablename,
-        b.btdrelpages as actual_pages,
-        b.btdexppages as expected_pages,
-        round(pg_relation_size(n.nspname || ‘.’ || c.relname)/1024/1024) actual_size_mb,
-        round(pg_relation_size(n.nspname || ‘.’ || c.relname)/1024/1024 * b.btdexppages/b.btdrelpages) expected_size_mb
-    FROM gp_toolkit.gp_bloat_expected_pages b
-    JOIN pg_class c ON c.relnamespace = n.oid
-    WHERE 1=1
-    -- AND b.btdrelpages > 1
-    AND n.nspname not in (‘pg_catalog’, 'gp_toolkit', 'information_schema') ) a
-WHERE 1=1
--- AND actual_size_mb > 1
--- AND actual_size_mb / expected_size_mb > 1
-ORDER BY 1,2 desc;"
+select a.*
+from (
+    select n.nspname as schemaname,
+            c.relname as tablename,
+            b.btdrelpages as actual_pages,
+            b.btdexppages as expected_pages,
+            round(pg_relation_size(n.nspname || '.' || c.relname)/1024/1024) as actual_size_mb,
+            round(pg_relation_size(n.nspname || '.' || c.relname)/1024/1024 * b.btdexppages/b.btdrelpages) as expected_size_mb
+    from gp_toolkit.gp_bloat_expected_pages b
+    join pg_class as c
+    on c.oid=b.btdrelid
+    join pg_namespace as n
+    on c.relnamespace=n.oid
+    where b.btdrelpages > 1
+    and n.nspname not in (‘pg_catalog’, 'gp_toolkit', 'information_schema') ) as a
+--where actual_size_mb > 1
+--and actual_size_mb / expected_size_mb > 1
+order by 1,2 desc ;" >> ${LOGFILE}
