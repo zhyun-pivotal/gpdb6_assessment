4f94e9788d83ab6a64e0b772a209f1fcbda71182
zhyun-pivotal <52663478+zhyun-pivotal@users.noreply.github.com>
2023-12-07 14:59:26 +0900

    Update chk_skew.sh

--- chk_skew.sh
+++ chk_skew.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir -p /home/gpadmin/dba/diaglog
+#mkdir -p /home/gpadmin/diag/diaglog
 export LOGFILE=/home/gpadmin/diag/diaglog/chk_skew.$(date '+%Y%m%d_%H%M')
 
 psql -f /home/gpadmin/dba/f_crt_view_chk_file_skew.sql
