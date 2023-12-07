248e59949fcb83a74db99e783a33fb9f05a54991
zhyun-pivotal <52663478+zhyun-pivotal@users.noreply.github.com>
2023-12-07 19:10:24 +0900

    Update run_diag.sh

--- run_diag.sh
+++ run_diag.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-echo"##############################"
-echo"# 1. Prepare directory"
-echo"##############################"
+echo "##############################"
+echo "# 1. Prepare directory"
+echo "##############################"
 mkdir -p /home/gpadmin/diag/diaglog
 mkdir -p /home/gpadmin/diag/csv
 
-echo"##############################"
-echo"# 2. Prepare hostfile"
-echo"##############################"
+echo "##############################"
+echo "# 2. Prepare hostfile"
+echo "##############################"
 psql -Atc "select hostname from gp_segment_configuration where content = -1 order by dbid;" > /home/gpadmin/diag/hostfile_master
 psql -Atc "select distinct(hostname) from gp_segment_configuration where content != -1 order by hostname;" > /home/gpadmin/diag/hostfile_seg
 psql -Atc "select distinct(hostname) from gp_segment_configuration order by hostname;" > /home/gpadmin/diag/hostfile_all
 
-echo"##############################"
-echo"# 3. Parameter assessment"
-echo"##############################"
+echo "##############################"
+echo "# 3. Parameter assessment"
+echo "##############################"
 sudo sh /home/gpadmin/diag/diag_os.sh
 sh /home/gpadmin/diag/diag_gpdb.sh
 
-echo"##############################"
-echo"# 4. DB upgrade assessment"
-echo"##############################"
+echo "##############################"
+echo "# 4. DB upgrade assessment"
+echo "##############################"
 sh /home/gpadmin/diag/diag_gpupgrade.sh
 
-echo"##############################"
-echo"# 5. System resource utilization assessment"
-echo"##############################"
+echo "##############################"
+echo "# 5. System resource utilization assessment"
+echo "##############################"
 sh /home/gpadmin/diag/chk_bloat_catalog.sh
 sh /home/gpadmin/diag/chk_bloat_tables.sh
 sh /home/gpadmin/diag/chk_skew.sh
