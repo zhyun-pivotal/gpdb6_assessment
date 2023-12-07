f648a7463cd966999308e0f04385596d5774db66
zhyun-pivotal <52663478+zhyun-pivotal@users.noreply.github.com>
2023-12-07 18:32:49 +0900

    Update run_diag.sh

--- run_diag.sh
+++ run_diag.sh
@@ -9,7 +9,7 @@ mkdir -p /home/gpadmin/diag/csv
 ##############################
 # 2. Prepare hostfile
 ##############################
-psql -Atc "select distinct(hostname) from gp_segment_configuration where content = -1 order by hostname;" > /home/gpadmin/diag/hostfile_master
+psql -Atc "select hostname from gp_segment_configuration where content = -1 order by dbid;" > /home/gpadmin/diag/hostfile_master
 psql -Atc "select distinct(hostname) from gp_segment_configuration where content != -1 order by hostname;" > /home/gpadmin/diag/hostfile_seg
 psql -Atc "select distinct(hostname) from gp_segment_configuration order by hostname;" > /home/gpadmin/diag/hostfile_all
 
